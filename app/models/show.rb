9c4ff5eccc1ad779c7ff5f4cb33d2031a689e1a7
edougher <ejdougher@gmail.com>
2021-01-01 22:40:00 -0500

    commit tree e3bb34a889a8adbd89219d4daf201f5cd0be5ff3

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.map do |character|
+            "#{character.actor.first_name} #{character.actor.last_name}"
+        end
+    end
+
+    # suggested solution
+    #self.actors.map{|actor| actor.full_name}
 end
